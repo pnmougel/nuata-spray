f06358fd9aa21a924a7b81ef74dd2d20ca917bbc
pnmougel <pnmougel@gmail.com>
2016-02-17 11:50:06 +0100

    Removed rejection handler to remove a bug

--- src/main/scala/org/nuata/actors/HttpActor.scala
+++ src/main/scala/org/nuata/actors/HttpActor.scala
@@ -13,19 +13,19 @@ import spray.routing.{MalformedRequestContentRejection, RejectionHandler}
 class HttpActor extends Actor with RouteService {
   def actorRefFactory = context
 
-  implicit def myRejectionHandler = RejectionHandler {
-    case MalformedRequestContentRejection(errorMsg, e) :: _ =>
-//      println(errorMsg)
-//      println(e)
-//      for(foo <- e) {
-//        println(foo.getClass)
-//        val mappingException = foo.asInstanceOf[MappingException]
-////        println(foo.getLocalizedMessage)
-////        println(foo.getMessage)
-////        foo.printStackTrace()
-//      }
-      complete(BadRequest, Extraction.decompose(Map("error" -> errorMsg)) )
-  }
+//  implicit def myRejectionHandler = RejectionHandler {
+//    case MalformedRequestContentRejection(errorMsg, e) :: _ =>
+////      println(errorMsg)
+////      println(e)
+////      for(foo <- e) {
+////        println(foo.getClass)
+////        val mappingException = foo.asInstanceOf[MappingException]
+//////        println(foo.getLocalizedMessage)
+//////        println(foo.getMessage)
+//////        foo.printStackTrace()
+////      }
+//      complete(BadRequest, Extraction.decompose(Map("error" -> errorMsg)) )
+//  }
 
   // this actor only runs our route, but you could add
   // other things here, like request stream processing
