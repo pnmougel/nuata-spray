056776371d7ff1631ab2936d05e89a62a48fb19e
pnmougel <pnmougel@gmail.com>
2016-02-20 12:17:02 +0100

    Added directive to retrieve user language

--- src/main/scala/org/nuata/services/routing/RouteProvider.scala
+++ src/main/scala/org/nuata/services/routing/RouteProvider.scala
@@ -6,5 +6,12 @@ import spray.routing._
  * Created by nico on 16/02/16.
  */
 trait RouteProvider extends HttpServiceBase {
+  val getLang = extract { ctx =>
+    ctx.request.headers
+      .filter(_.is("accept-language"))
+      .map(_.value)
+      .headOption
+  }
+
   def route(implicit settings : spray.routing.RoutingSettings, refFactory : akka.actor.ActorRefFactory) : Route
 }
